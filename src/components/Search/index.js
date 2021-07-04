d118b994068a3aadaa0d5ab8f9f1d49f1c8e7333
Nayane Santos <nayanemenezes2011@gmail.com>
2021-07-04 20:41:25 -0300

    :up: atualização

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,53 +1,27 @@
-import React, {useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { requestHTTP } from '../../store/actions';
-import CardItem from '../Card/CardItem';
+import React from 'react';
 import {SearchContainer, SearchContent, FormSearch,InputSearch,FaSearchCustom} from './SearchElements'
 
-function Search () {
-    const dispatch = useDispatch();
-    const state = useSelector((state) => state.products);
-    const [searchText, setSearchText] = useState('');
 
-    useEffect(() => {
-      dispatch(requestHTTP());
-    }, []);
-
-    const [projectsData, setProjectsData] = useState(`${state.products.products}`);
     
-    useEffect(() => {
-      if (searchText === '') return;
-      setProjectsData(() =>
-        state.products.products.filter((item) =>
-          item.name.toLowerCase().match(searchText.toLowerCase())
-        )
-      );
-    }, [searchText]);
-    const handleChange = (e) => {
-      e.preventDefault();
-      setSearchText(e.target.value);
-      if (!e.target.value.length > 0) {
-        setProjectsData(`${state.products.products}`);
-      }
-    };
+function Search({value, onChange}) {
+  
+
+  function handleChange(event) {
+      onChange(event.target.value)
+  }
     return(
         <SearchContainer>
         <SearchContent>
         <FormSearch>
           <InputSearch
-            type="text"
-            value={searchText}
+            type='search' 
+            value={value} 
             onChange={handleChange}
             placeholder="Busque por um produto"
           />
           <FaSearchCustom />
         </FormSearch>
         </SearchContent>
-        {/* <div>
-            {state.products.products.map((item) => (
-              <CardItem key={item.ean} product={item} />                
-            ))}
-        </div> */}
         </SearchContainer>
     );
 }
--- src/store/sagas/products.js
+++ src/store/sagas/products.js
@@ -6,16 +6,20 @@ import Api from '../../api'
      try {
 
          const {data} = yield call(Api.get, 'recruitment/products/')
-         yield put(AddList(data))
-     } catch (error) {
-         
+        //  yield put(AddList(data))
+          yield put({ type: 'SUCCESS_PRODUCTS_TO_LIST', payload: (AddList(data)) });
+        
+     } catch (err) {
+        // yield put({ type: 'FAILURE_PRODUCTS_TO_LIST' });
      }finally {
 
      }
 
 }
 
-export default all([
+
+
+export default  all([
     takeLatest('FETCH_PRODUCTS',List),
 
 ])
